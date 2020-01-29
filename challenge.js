58b52654298f712e42d0b3344b39ad550338c6c3
RachelIsrael <racheleisrael4@gmail.com>
2020-01-29 17:29:22 -0500

    deck arrays 2

--- challenge.js
+++ challenge.js
@@ -1,1 +1,12 @@
-let deck =[]
+let deck =[];
+let suits=["♠️", "♣️", "♥️", "♦️"];
+let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+function getDeck(){
+    for (let i=0; i<suits.length; i++){
+        for(let j=0; j<value.length; j++){
+            let card = {Value: values[j], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+    return deck;
+}
