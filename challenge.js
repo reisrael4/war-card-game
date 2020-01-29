d8a666cec4d590fb76cc6a6ae7aa8be6c9bdf263
RachelIsrael <racheleisrael4@gmail.com>
2020-01-29 18:18:01 -0500

    edited gameplay functions

--- challenge.js
+++ challenge.js
@@ -3,7 +3,8 @@ let suits=["♠️", "♣️", "♥️", "♦️"];
 let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 let player1=[];
 let player2=[];
-let cardsInPlay=[];
+let cardsInPlay1=[];
+let cardsInPlay2=[];
 function createDeck(){
     for (let i=0; i<suits.length; i++){
         for(let j=0; j<values.length; j++){
@@ -13,26 +14,29 @@ function createDeck(){
     }
     return mainDeck;
 }
+function compare(){
+    if(cardsInPlay1[0].rank > cardsInPlay2[0].rank){
+        player1.push(cardsInPlay1[0], cardsInPlay2[0]);
+        cardsInPlay1.pop();
+        cardsInPlay2.pop();
+    }
+    else if(cardsInPlay1[0].rank < cardsInPlay2[0].rank){
+        player2.push(cardsInPlay1[0], cardsInPlay2[0]);
+        cardsInPlay1.pop();
+        cardsInPlay2.pop();
+    }
+    else{
+        war();
+    }
+}
+function war(){
+
+};
+
 function playRound(){
-    cardsInPlay.push(player1[0]);
+    cardsInPlay1.push(player1[0]);
     player1.shift();
-    cardsInPlay.push(player2[0]);
+    cardsInPlay1.push(player2[0]);
     player2.shift();
     compare();
     }
-}
-function compare(){
-    if(cardsInPlay[0].rank > cardsInPlay[1].rank){
-        player1.push(cardsInPlay[0], cardsInPlay[1]);
-        cardsInPlay.pop();
-        cardsInPlay.pop();
-    }
-    else if(cardsInPlay[0].rank < cardsInPlay[1].rank){
-        player2.push(cardsInPlay[0], cardsInPlay[1]);
-        cardsInPlay.pop();
-        cardsInPlay.pop();
-    }
-    else{
-        function war(){};
-    }
-}
