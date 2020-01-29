273d21842aa403f9e09af8e10a581467f39a7a6c
RachelIsrael <racheleisrael4@gmail.com>
2020-01-29 18:12:14 -0500

    initial gameplay functions

--- challenge.js
+++ challenge.js
@@ -3,6 +3,7 @@ let suits=["♠️", "♣️", "♥️", "♦️"];
 let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 let player1=[];
 let player2=[];
+let cardsInPlay=[];
 function createDeck(){
     for (let i=0; i<suits.length; i++){
         for(let j=0; j<values.length; j++){
@@ -11,4 +12,27 @@ function createDeck(){
         }
     }
     return mainDeck;
+}
+function playRound(){
+    cardsInPlay.push(player1[0]);
+    player1.shift();
+    cardsInPlay.push(player2[0]);
+    player2.shift();
+    compare();
+    }
+}
+function compare(){
+    if(cardsInPlay[0].rank > cardsInPlay[1].rank){
+        player1.push(cardsInPlay[0], cardsInPlay[1]);
+        cardsInPlay.pop();
+        cardsInPlay.pop();
+    }
+    else if(cardsInPlay[0].rank < cardsInPlay[1].rank){
+        player2.push(cardsInPlay[0], cardsInPlay[1]);
+        cardsInPlay.pop();
+        cardsInPlay.pop();
+    }
+    else{
+        function war(){};
+    }
 }
