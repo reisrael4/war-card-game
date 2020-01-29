585294c6e68636418b05722416b739ae922c36dd
RachelIsrael <racheleisrael4@gmail.com>
2020-01-29 17:51:56 -0500

    creat deck

--- challenge.js
+++ challenge.js
@@ -1,12 +1,14 @@
-let deck =[];
+let mainDeck =[];
 let suits=["♠️", "♣️", "♥️", "♦️"];
 let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-function getDeck(){
+let player1=[];
+let player2=[];
+function createDeck(){
     for (let i=0; i<suits.length; i++){
-        for(let j=0; j<value.length; j++){
-            let card = {Value: values[j], Suit: suits[i]};
-            deck.push(card);
+        for(let j=0; j<values.length; j++){
+            let card = {Value: values[j], Suit: suits[i], rank: j};
+            mainDeck.push(card);
         }
     }
-    return deck;
+    return mainDeck;
 }
