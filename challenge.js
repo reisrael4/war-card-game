76e2200ea1952919f2e5af465d17f18a1ba9336d
RachelIsrael <racheleisrael4@gmail.com>
2020-02-02 13:51:59 -0500

    OOP

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-let mainDeck =[];
+// let mainDeck =[];
 let suits=["♠️", "♣️", "♥️", "♦️"];
 let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 let player1=[];
@@ -9,49 +9,96 @@ let cardsInPlay2=[];
 let button = document.getElementsByClassName("play-game");
 addEventListener("click", playGame);
 
-function createDeck(){
-    for (let i=0; i<suits.length; i++){
-        for(let j=0; j<values.length; j++){
-            let card = {Value: values[j], Suit: suits[i], rank: j};
-            mainDeck.push(card);
-        }
+class Card{
+    constructor(suit, rank, score){
+        this.suit = suit
+        this.rank = rank
+        this.score = score
     }
-    return mainDeck;
-}
-function playGame(){
-    deal();
-    playRound();
-}
-function shuffle(){
+};
+class Deck{
+    constructor(){
+        this.cards = [];
+    }
+    addCards(){
+       let suits = ['♥️', '♦️', '♣️', '♠️'];
+       let ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+       let scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+       for(let i=0; i<suits.length; i++){
+           for(let j=0; j<ranks.length; j++){
+               this.cards.push(new Card(suits[i], ranks[j], scores[j]))
+           }
+       }
+   }
+   shuffle(){
     //Fisher-Yates Shuffle
-    for(let i=mainDeck.length - 1; i > 0; i-=1){ 
+    for(let i=this.cards.length - 1; i > 0; i-=1){ 
         let j = Math.floor(Math.random() * (i + 1));
-        let temp = mainDeck[i];
-        mainDeck[i] = mainDeck[j];
-        mainDeck[j] = temp
+        let temp = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = temp
     }  
-    return mainDeck;
+    return this.cards;
 }
-function deal(){
-    shuffle();
-        for(i=0; i<mainDeck.length; i++){
+    deal(){
+        for(let i=0; i<this.cards.length; i++){
             if(i % 2 === 0){
-            player1.push(mainDeck[i])
+            player1.push(this.cards[i])
         } else{
-            player2.push(mainDeck[i])
+            player2.push(this.cards[i])
         }
     }
-    return [player1, player2]
 }
+};
+
+const mainDeck = new Deck()
+mainDeck.addCards();
+mainDeck.shuffle();
+mainDeck.deal();
+
+// function createDeck(){
+//     for (let i=0; i<suits.length; i++){
+//         for(let j=0; j<values.length; j++){
+//             let card = {Value: values[j], Suit: suits[i], rank: j};
+//             mainDeck.push(card);
+//         }
+//     }
+//     return mainDeck;
+// }
+// function playGame(){
+    // deal();
+//     playRound();
+// }
+// function shuffle(){
+//     //Fisher-Yates Shuffle
+//     for(let i=mainDeck.length - 1; i > 0; i-=1){ 
+//         let j = Math.floor(Math.random() * (i + 1));
+//         let temp = mainDeck[i];
+//         mainDeck[i] = mainDeck[j];
+//         mainDeck[j] = temp
+//     }  
+//     return mainDeck;
+// }
+// function deal(){
+//     // shuffle();
+//         for(i=0; i<mainDeck.length; i++){
+//             if(i % 2 === 0){
+//             player1.push(mainDeck[i])
+//         } else{
+//             player2.push(mainDeck[i])
+//         }
+//     }
+//     return [player1, player2]
+// }
 function compare(){
-    if(cardsInPlay1[0].rank > cardsInPlay2[0].rank){
+    if(cardsInPlay1[0].score > cardsInPlay2[0].score){
         player1.push(cardsInPlay1[0], cardsInPlay2[0]);
         cardsInPlay1 = [];
         cardsInPlay2 = [];
         console.log(`Player 1 wins this round! Player 1 has ${player1.length} cards! Player 2 has ${player2.length} cards!`)
         //Player 1 flipped the ${cardsInPlay1[0].value} of ${cardsInPlay1[0].suit}. Player 2 flipped the ${cardsInPlay2[0].value} of ${cardsInPlay2[0].suit}. 
     }
-    else if(cardsInPlay1[0].rank < cardsInPlay2[0].rank){
+    else if(cardsInPlay1[0].score < cardsInPlay2[0].score){
         player2.push(cardsInPlay2[0], cardsInPlay1[0]);
         cardsInPlay1 = [];
         cardsInPlay2 = [];
@@ -63,13 +110,13 @@ function compare(){
     }
 }
 function compareWar(){
-    if(cardsInPlay1[0].rank > cardsInPlay2[0].rank){
+    if(cardsInPlay1[0].score > cardsInPlay2[0].score){
         player1.push(...cardsInPlay1, ...cardsInPlay2);
         cardsInPlay1 = [];
         cardsInPlay2 = [];
         console.log(`Player 1 wins this war! Player 1 has ${player1.length} cards! Player 2 has ${player2.length} cards!`)
     }
-    else if(cardsInPlay1[0].rank < cardsInPlay2[0].rank){
+    else if(cardsInPlay1[0].score < cardsInPlay2[0].score){
         player2.push(...cardsInPlay2, ...cardsInPlay1);
         cardsInPlay1 = [];
         cardsInPlay2 = [];
@@ -92,24 +139,27 @@ function flipCard(){
     cardsInPlay2.push(player2[0]);
     player2.shift();
     }
-function playRound(){
+function playGame(){
     flipCard();
-    console.log(`Player 1 flipped the ${cardsInPlay1[0].Value} of ${cardsInPlay1[0].Suit}. Player 2 flipped the ${cardsInPlay2[0].Value} of ${cardsInPlay2[0].Suit}.`);
+    console.log(`Player 1 flipped the ${cardsInPlay1[0].rank} of ${cardsInPlay1[0].suit}. Player 2 flipped the ${cardsInPlay2[0].rank} of ${cardsInPlay2[0].suit}.`);
     compare();
     checkForWinner();
 }
 function checkForWinner(){
     if(player1.length===52){
         player1 = [];
-        shuffle();
+        mainDeck.shuffle();
+        mainDeck.deal();
         console.log("Congratulations Player 1!")
     } else if(player2.length===52){
         player2 = [];
-        shuffle();
+        mainDeck.shuffle();
+        mainDeck.deal();
         console.log("Congratulations Player 2!")
     } else{ 
-       playRound(); 
+       playGame(); 
     }
 }
-createDeck();
+// createDeck();
+
 playGame();
