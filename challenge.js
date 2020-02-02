111ee194e38bc8acd9be269719b3f4d6cf867313
RachelIsrael <racheleisrael4@gmail.com>
2020-02-02 14:59:25 -0500

    aces high

--- challenge.js
+++ challenge.js
@@ -1,6 +1,6 @@
 // let mainDeck =[];
-let suits=["♠️", "♣️", "♥️", "♦️"];
-let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+// let suits=["♠️", "♣️", "♥️", "♦️"];
+// let values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 let player1=[];
 let player2=[];
 let cardsInPlay1=[];
@@ -22,7 +22,7 @@ class Deck{
     }
     addCards(){
        let suits = ['♥️', '♦️', '♣️', '♠️'];
-       let ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+       let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
        let scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
        for(let i=0; i<suits.length; i++){
            for(let j=0; j<ranks.length; j++){
